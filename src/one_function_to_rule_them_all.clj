feb56b0cb9c78212c258b5a4bb87270ea4f3ad1c
Ilmo Raunio <ilmo.raunio@gmail.com>
2017-05-29 22:06:48 +0300

    my-interpose to pred-and: answers

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -11,37 +11,57 @@
   (reduce str (concat-elements (interpose " " a-seq))))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (reduce #(if (empty? %1)
+            (conj %1 %2)
+            (conj %1 x %2))
+    [] a-seq))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [a _] (inc a)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce #(conj %1 %2) () a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (reduce (fn [[min max :as a] b]
+            (if (empty? a)
+              [b b]
+              (let [min (if (< b min) b min)
+                    max (if (> b max) b max)]
+                [min max]))) () a-seq))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (concat (take-while #(< % n) sorted-seq)
+          (list n)
+          (drop-while #(> n %) sorted-seq)))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce #(insert %1 %2) () a-seq))
 
 (defn parity [a-seq]
-  [:-])
+  (reduce #(if (contains? %1 %2)
+            (disj %1 %2)
+            (conj %1 %2)) #{} a-seq))
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x] (* -1 x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params [& more]
+  (count more))
 
-(defn my-* [x]
-  :-)
+(defn my-*
+  ([] 1)
+  ([x] x)
+  ([x y] (* 4 3))
+  ([x y & more] (reduce * (* x y) more)))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and
+  ([] (fn [x] true))
+  ([p?] p?)
+  ([p1? p2?] (fn [x] (and (p1? x) (p2? x))))
+  ([p1? p2? & preds] (fn [x]
+                       (reduce (fn [a b] (and a (b x))) (and (p1? x) (p2? x)) preds))))
 
 (defn my-map [f a-seq]
   [:-])
